4383b6345e0fe7bdb1184899661ceefb83d411b6
pyVarad <py.varad@gmail.com>
2021-05-23 19:02:25 +0530

    Updated uwsgi

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from app import app
+from app import app as application
 from db import db
 
-db.init_app(app)
-@app.before_first_request
+db.init_app(application)
+@application.before_first_request
 def create_db():
     db.create_all()
